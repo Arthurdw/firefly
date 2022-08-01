70c268c076f8a6b41053d942757b8956507d029b
Arthurdw <dev@arthurdw.com>
2022-08-02 01:12:53 +0200

    :construction: Started working on the query lang and tcp connector

--- README.md
+++ README.md
@@ -4,6 +4,132 @@ An optimized tweaked key-value pair database. It is a simple, fast, and secure.
 At [Xiler](https://www.xiler.net) it gets used to store and manage client
 sessions throughout the platform.
 
+## Query Language
+
+Firefly only has three data operators, `NEW`, `GET`, `DROP`. We'll walk over
+each one.
+
+### Important note on values
+
+ALL values must be valid ASCII, if not the server will reject the query.
+
+### Defining query types
+
+The server may only accept one query type per TCP connection type. The default
+type is a string query. But this can be changed by using the `QUERY TYPE`
+keyword.
+
+```ffly
+QUERY TYPE [STRING | BITWISE];
+```
+
+### String queries
+
+The simplest way to query something is by querying it using a string query. But
+because the parsing, and bandwidth of this is more than that is necessary we
+also provide bitwise queries.
+
+#### Create
+
+You can create a new record by using the `NEW` keyword, the arguments should be
+wrapped within quotes. The first argument _(after `NEW`)_ is the key, this
+should be unique throughout the db. If no TLL value is provided, the server
+will use 0 _(aka no expiry)_.
+
+```ffly
+NEW '{key}'
+VALUE '{value}'
+[ WITH TTL {ttl}];
+```
+
+##### Create examples
+
+```ffly
+NEW 's2.8eqYursP2McHeQvHB2bauyE6n3vptOj8M96PxmGAQMDfimeZ31WAzP3hSw5Ixv5Y'
+VALUE '86ebe1a0-11bf-11ed-aa8e-13602e2ad46b';
+```
+
+```ffly
+NEW 's2.8eqYursP2McHeQvHB2bauyE6n3vptOj8M96PxmGAQMDfimeZ31WAzP3hSw5Ixv5Y'
+VALUE '86ebe1a0-11bf-11ed-aa8e-13602e2ad46b'
+WITH TTL 604800;
+```
+
+#### Fetch
+
+The `GET` keyword returns the value and TTL by default. But if you only want
+one of the two, you can specify this. You can only search by key!
+
+```ffly
+GET [VALUE | TTL] '{key}';
+```
+
+##### Fetch examples
+
+```ffly
+GET 's2.8eqYursP2McHeQvHB2bauyE6n3vptOj8M96PxmGAQMDfimeZ31WAzP3hSw5Ixv5Y';
+```
+
+```ffly
+GET TTL 's2.8eqYursP2McHeQvHB2bauyE6n3vptOj8M96PxmGAQMDfimeZ31WAzP3hSw5Ixv5Y';
+```
+
+#### Delete
+
+Deleting one record is as straightforward as fetching one. You can only delete
+whole records. If required all records that have a value can also be deleted,
+but this action is very expensive and generally not recommended.
+
+```ffly
+DROP '{key}';
+DROPALL '{value}';
+```
+
+### Bitwise queries
+
+Because string queries can consume more resources than what is required, there
+is a more efficient _(less friendly)_ way to interact with Firefly. This is by
+sending the bits in a specific format. This section just describes the formats,
+if you want more information about the queries itself, then you can find it in
+the `String queries` section.
+
+Please keep in mind that all the bitwise queries are very strict, and if a
+query is unrecognized it will discard it.
+
+#### General notes
+
+-   All values are delimited by a NUL character. _(`0x0`)_
+-   The end of the query is assumed to be the last byte.
+-   Queries start with their type, this is a numeric value
+    -   0: `NEW`
+    -   1: `GET`
+    -   2: `GET VALUE`
+    -   3: `GET TTL`
+    -   4: `DROP`
+    -   5: `DROPALL`
+-   The query type does not need to be delimited
+
+#### Bitwise create
+
+A with TTL must always be provided. If you don't want a TTL set this to 0.
+
+`0{key}0x0{value}0x0{ttl}`
+
+These are the two same create examples from the string queries:
+`0s2.8eqYursP2McHeQvHB2bauyE6n3vptOj8M96PxmGAQMDfimeZ31WAzP3hSw5Ixv5Y0x086ebe1a0-11bf-11ed-aa8e-13602e2ad46b0x0`
+`0s2.8eqYursP2McHeQvHB2bauyE6n3vptOj8M96PxmGAQMDfimeZ31WAzP3hSw5Ixv5Y0x086ebe1a0-11bf-11ed-aa8e-13602e2ad46b0x0602800`
+
+#### Bitwise fetch
+
+`1s2.8eqYursP2McHeQvHB2bauyE6n3vptOj8M96PxmGAQMDfimeZ31WAzP3hSw5Ixv5Y`
+`2s2.8eqYursP2McHeQvHB2bauyE6n3vptOj8M96PxmGAQMDfimeZ31WAzP3hSw5Ixv5Y`
+`3s2.8eqYursP2McHeQvHB2bauyE6n3vptOj8M96PxmGAQMDfimeZ31WAzP3hSw5Ixv5Y`
+
+#### Bitwise delete
+
+`4s2.8eqYursP2McHeQvHB2bauyE6n3vptOj8M96PxmGAQMDfimeZ31WAzP3hSw5Ixv5Y`
+`5s2.8eqYursP2McHeQvHB2bauyE6n3vptOj8M96PxmGAQMDfimeZ31WAzP3hSw5Ixv5Y`
+
 ## Inner working
 
 ### Serialization
@@ -12,7 +138,7 @@ Firefly uses a custom minimalist serialization format. Since all values are
 always present, we can just add one delimiter byte to the start of each
 partition.
 
-The delimiter byte is a `NULL` byte, this byte is not allowed to be within the
+The delimiter byte is a `NUL` byte, this byte is not allowed to be within the
 content of the message.
 
 ### ASCII optimization
--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,120 @@
-use std::{collections::HashMap, fs::File, io::Write, time::Instant};
+extern crate pretty_env_logger;
 
-use crate::ascii_optimisation::{compress_slice, decompress_slice};
-use crate::serialisation::{from_slice, to_vec, Map};
+#[macro_use]
+extern crate log;
 
-mod ascii_optimisation;
-mod serialisation;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
 
-#[cfg(test)]
-mod test_serialisation;
+use std::env;
+use std::error::Error;
 
-#[cfg(test)]
-mod test_ascii_optimisation;
+static LOGGING_ENV: &'static str = "LOG_LEVEL";
+static BIND_ADDR: &'static str = "127.0.0.1:46600";
 
-fn main() {
-    // let sample: Map = HashMap::from([
-    //     (
-    //         "hello".to_string(),
-    //         ("world".to_string(), "2022-04-12T22:10:57+02:00".to_string()),
-    //     ),
-    //     (
-    //         "foo".to_string(),
-    //         ("bar".to_string(), "2022-04-12T22:10:57+02:00".to_string()),
-    //     ),
-    //     (
-    //         "bake".to_string(),
-    //         ("eggs".to_string(), "2022-04-12T22:10:57+02:00".to_string()),
-    //     ),
-    // ]);
-    let sample: Map = {
-        use rand::{distributions::Alphanumeric, Rng};
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    if env::var_os(LOGGING_ENV).is_none() {
+        env::set_var(LOGGING_ENV, "INFO");
+    }
 
-        let mut map: Map = HashMap::new();
+    pretty_env_logger::init_custom_env(LOGGING_ENV);
 
-        for i in 0..50_000 {
-            let key = format!("key{}", i);
-            let value = {
-                let s: String = rand::thread_rng()
-                    .sample_iter(&Alphanumeric)
-                    .take(64)
-                    .map(char::from)
-                    .collect();
+    let listener = TcpListener::bind(BIND_ADDR).await?;
+    info!("Listening on: {}", BIND_ADDR);
 
-                (s, "2100-07-26T14:17:06+00:00".to_string())
-            };
-            map.insert(key, value);
-        }
+    loop {
+        let (mut socket, _) = listener.accept().await?;
 
-        map
-    };
+        tokio::spawn(async move {
+            let mut buf = vec![0; 1024];
 
-    println!("Firefly specific encoder:");
-    println!("Applying tests to {} items...", sample.len());
+            loop {
+                let n = socket
+                    .read(&mut buf)
+                    .await
+                    .expect("failed to read data from socket");
 
-    let start = Instant::now();
-    let buffer = to_vec(sample).unwrap();
-    let elapsed = start.elapsed();
-    println!(
-        "Serialisation time: {:.2?}, size: {:?}",
-        elapsed,
-        &buffer.len()
-    );
+                if n == 0 {
+                    return;
+                }
 
-    let compressed_start = Instant::now();
-    let compressed = compress_slice(&buffer).unwrap();
-    let compressed_elapsed = compressed_start.elapsed();
-    println!(
-        "Compression time: {:.2?}, size: {:?}",
-        compressed_elapsed,
-        compressed.len()
-    );
-
-    let decompression_start = Instant::now();
-    let decompressed = decompress_slice(&compressed);
-    let decompression_elapsed = decompression_start.elapsed();
-    println!("Decompression time: {:.2?}", decompression_elapsed);
-
-    let decode_start = Instant::now();
-    let _decoded = from_slice(&decompressed).unwrap();
-    let decode_elapsed = decode_start.elapsed();
-    println!("Deserialisation time: {:.2?}", decode_elapsed);
-
-    // Write buffer to file
-    let mut file = File::create("messages.ffly").unwrap();
-    file.write(&compressed).unwrap();
+                info!("{:?}", String::from_utf8_lossy(&buf[..n]));
+                socket
+                    .write_all(&buf[0..n])
+                    .await
+                    .expect("failed to write data to socket");
+            }
+        });
+    }
 }
+// use std::{collections::HashMap, fs::File, io::Write, time::Instant};
+//
+// use crate::ascii_optimisation::{compress_slice, decompress_slice};
+// use crate::serialisation::{from_slice, to_vec, Map};
+//
+// mod ascii_optimisation;
+// mod serialisation;
+//
+// #[cfg(test)]
+// mod test_serialisation;
+//
+// #[cfg(test)]
+// mod test_ascii_optimisation;
+//
+// fn main() {
+//     let sample: Map = {
+//         use rand::{distributions::Alphanumeric, Rng};
+//
+//         let mut map: Map = HashMap::new();
+//
+//         for i in 0..50 {
+//             let key: String = rand::thread_rng()
+//                 .sample_iter(&Alphanumeric)
+//                 .take(64)
+//                 .map(char::from)
+//                 .collect();
+//             let value = (i.to_string(), "2100-07-26T14:17:06+00:00".to_string());
+//
+//             map.insert(key, value);
+//         }
+//
+//         map
+//     };
+//
+//     println!("Firefly specific encoder:");
+//     println!("Applying tests to {} items...", sample.len());
+//     println!("{:?}", sample);
+//
+//     let start = Instant::now();
+//     let buffer = to_vec(sample).unwrap();
+//     let elapsed = start.elapsed();
+//     println!(
+//         "Serialisation time: {:.2?}, size: {:?}",
+//         elapsed,
+//         &buffer.len()
+//     );
+//
+//     let compressed_start = Instant::now();
+//     let compressed = compress_slice(&buffer).unwrap();
+//     let compressed_elapsed = compressed_start.elapsed();
+//     println!(
+//         "Compression time: {:.2?}, size: {:?}",
+//         compressed_elapsed,
+//         compressed.len()
+//     );
+//
+//     let decompression_start = Instant::now();
+//     let decompressed = decompress_slice(&compressed);
+//     let decompression_elapsed = decompression_start.elapsed();
+//     println!("Decompression time: {:.2?}", decompression_elapsed);
+//
+//     let decode_start = Instant::now();
+//     let _decoded = from_slice(&decompressed).unwrap();
+//     let decode_elapsed = decode_start.elapsed();
+//     println!("Deserialisation time: {:.2?}", decode_elapsed);
+//
+//     // Write buffer to file
+//     let mut file = File::create("messages.ffly").unwrap();
+//     file.write(&compressed).unwrap();
+// }
